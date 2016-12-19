146a3022795d2c42f8452ee78d9941235b46c959
Bill Seremetis <bill@seremetis.net>
2016-12-19 03:12:03 +0200

    User authentication. Bootstrap on Chrome working.

--- arduino_home_automation.ino
+++ arduino_home_automation.ino
@@ -20,6 +20,9 @@ DaiseikaiHeatpumpIR *heatpumpIR;
 #include <SPI.h>
 //#include "avr/pgmspace.h"
 #include <Ethernet.h>
+/* you can change the authentication realm by defining
+ * WEBDUINO_AUTH_REALM before including WebServer.h */
+#define WEBDUINO_AUTH_REALM "Climate control requires god mode!"
 #include <WebServer.h>
 static uint8_t mac[] = {
   0x74, 0x69, 0x69, 0x2D, 0x30, 0x31 };  // W5100 MAC address
@@ -34,7 +37,7 @@ WebServer webserver(PREFIX, 9093);
 P(indexpage) = "<!DOCTYPE html>"
 "<html>"
 "  <head>"
-"    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>"
+"    <link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>"
 "  </head>"
 "  <body>"
 "    <div class='container'>"
@@ -126,7 +129,7 @@ P(indexpage) = "<!DOCTYPE html>"
 P(formactionpage_header) = "<!DOCTYPE html>"
 "<html>"
 "  <head>"
-"    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>"
+"    <link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>"
 "  </head>"
 "<body>"
 "<h1>Parameters</h1>";
@@ -145,50 +148,66 @@ void indexCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail,
 // The send_ir.html. This handles the parameters submitted by the form
 void ACCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete)
 {
-  URLPARAM_RESULT rc;
-  char name[NAMELEN];
-  char value[VALUELEN];
-  int param = 0;
-
-  // Sensible defaults for the heat pump mode
-
-  byte powerState    = POWER_OFF;
-  byte operatingMode = MODE_AUTO;
-  byte fanSpeed      = FAN_AUTO;
-  byte temperature   = DEFAULT_TEMPERATURE;
-  byte swingV        = VDIR_AUTO;
-  byte swingH        = HDIR_AUTO;
-
-  server.httpSuccess();
-  server.printP(formactionpage_header);
-
-  if (strlen(url_tail))
+  /* username = user
+   * password = user
+   * display a page saying "Hello User"
+   *
+   * the credentials have to be concatenated with a colon like
+   * username:password
+   * and encoded using Base64 - this should be done outside of your Arduino
+   * to be easy on your resources
+   *
+   * in other words: "dXNlcjp1c2Vy" is the Base64 representation of "user:user"
+   */
+  if (server.checkCredentials("dXNlcjp1c2Vy"))
+  {
+    server.httpSuccess();
+    if (type != WebServer::HEAD)
     {
-    while (strlen(url_tail))
+      URLPARAM_RESULT rc;
+      char name[NAMELEN];
+      char value[VALUELEN];
+      int param = 0;
+
+      // Sensible defaults for the heat pump mode
+
+      byte powerState    = POWER_OFF;
+      byte operatingMode = MODE_AUTO;
+      byte fanSpeed      = FAN_AUTO;
+      byte temperature   = DEFAULT_TEMPERATURE;
+      byte swingV        = VDIR_AUTO;
+      byte swingH        = HDIR_AUTO;
+
+      server.httpSuccess();
+      server.printP(formactionpage_header);
+
+      if (strlen(url_tail))
       {
-      rc = server.nextURLparam(&url_tail, name, NAMELEN, value, VALUELEN);
-      if (rc != URLPARAM_EOS)
+        while (strlen(url_tail))
         {
-          if (strcmp(name, "power") == 0 )
+          rc = server.nextURLparam(&url_tail, name, NAMELEN, value, VALUELEN);
+          if (rc != URLPARAM_EOS)
           {
-            param = atoi(value);
- 
-            switch (param)
+            if (strcmp(name, "power") == 0 )
             {
+              param = atoi(value);
+
+              switch (param)
+              {
               case 0:
                 powerState = POWER_OFF;
                 break;
               case 1:
                 powerState = POWER_ON;
                 break;
+              }
             }
-          }
-          else if (strcmp(name, "mode") == 0 )
-          {
-            param = atoi(value);
-            
-            switch (param)
+            else if (strcmp(name, "mode") == 0 )
             {
+              param = atoi(value);
+
+              switch (param)
+              {
               case 1:
                 operatingMode = MODE_AUTO;
                 break;
@@ -204,14 +223,14 @@ void ACCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bo
               case 5:
                 operatingMode = MODE_FAN;
                 break;
+              }
             }
-          }
-          else if (strcmp(name, "fan") == 0 )
-          {
-            param = atoi(value);
-            
-            switch (param)
+            else if (strcmp(name, "fan") == 0 )
             {
+              param = atoi(value);
+
+              switch (param)
+              {
               case 1:
                 fanSpeed = FAN_AUTO;
                 break;
@@ -230,22 +249,22 @@ void ACCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bo
               case 6:
                 fanSpeed = FAN_5;
                 break;
+              }
             }
-          }
-          else if (strcmp(name, "temperature") == 0 )
-          {
-            param = atoi(value);
-            if ( param >= 15 && param <= 31 && temperature == DEFAULT_TEMPERATURE)
+            else if (strcmp(name, "temperature") == 0 )
             {
-              temperature = param;
+              param = atoi(value);
+              if ( param >= 15 && param <= 31 && temperature == DEFAULT_TEMPERATURE)
+              {
+                temperature = param;
+              }
             }
-          }
-          else if (strcmp(name, "vswing") == 0 )
-          {
-            param = atoi(value);
-        
-            switch (param)
+            else if (strcmp(name, "vswing") == 0 )
             {
+              param = atoi(value);
+
+              switch (param)
+              {
               case 1:
                 swingV = VDIR_AUTO;
                 break;
@@ -264,14 +283,14 @@ void ACCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bo
               case 6:
                 swingV = VDIR_DOWN;
                 break;
+              }
             }
-          }
-          else if (strcmp(name, "hswing") == 0 )
-          {
-            param = atoi(value);
-
-            switch (param)
+            else if (strcmp(name, "hswing") == 0 )
             {
+              param = atoi(value);
+
+              switch (param)
+              {
               case 1:
                 swingH = HDIR_AUTO;
                 break;
@@ -290,33 +309,40 @@ void ACCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bo
               case 6:
                 swingH = HDIR_MRIGHT;
                 break;
-            }
-          }          
-        server.print(name);
-        server.print(" = ");
-        server.print(param);
-        server.print("<br>");
+              }
+            }          
+            server.print(name);
+            server.print(" = ");
+            server.print(param);
+            server.print("<br>");
+          }
         }
       }
-    }
 
-  server.printP(formactionpage_footer);
-/*  
-  Serial.println("Values:");
-  Serial.println("Power: ");
-  Serial.println(powerState);
-  Serial.println("Mode: ");
-  Serial.println(operatingMode);
-  Serial.println("Fan: ");
-  Serial.println(fanSpeed);
-  Serial.println("Temp: ");
-  Serial.println(temperature);
-  Serial.println("SwingV: ");
-  Serial.println(swingV);
-  Serial.println("SwingH: ");
-  Serial.println(swingH);
-*/
-  heatpumpIR->send(irSender, powerState, operatingMode, fanSpeed, temperature, swingV, swingH);
+      server.printP(formactionpage_footer);
+      /*  
+       Serial.println("Values:");
+       Serial.println("Power: ");
+       Serial.println(powerState);
+       Serial.println("Mode: ");
+       Serial.println(operatingMode);
+       Serial.println("Fan: ");
+       Serial.println(fanSpeed);
+       Serial.println("Temp: ");
+       Serial.println(temperature);
+       Serial.println("SwingV: ");
+       Serial.println(swingV);
+       Serial.println("SwingH: ");
+       Serial.println(swingH);
+       */
+      heatpumpIR->send(irSender, powerState, operatingMode, fanSpeed, temperature, swingV, swingH);
+    }
+  }
+  else
+  {
+    /* send a 401 error back causing the web browser to prompt the user for credentials */
+    server.httpUnauthorized();
+  }
 }
 
 // The setup
@@ -352,4 +378,5 @@ void loop()
   // process incoming connections one at a time forever
   webserver.processConnection(buff, &len);
 }
+
 
