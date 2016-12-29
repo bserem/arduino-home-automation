6da625302d44a83dabab2c17708cb8b4bfd8bfc2
Bill Seremetis <bill@seremetis.net>
2016-12-29 13:41:56 +0200

    Update code to follow master of HeatpumpIR library.

--- arduino_home_automation.ino
+++ arduino_home_automation.ino
@@ -10,11 +10,11 @@
 
 // Daiseikai IR
 #include <Arduino.h>
-#include <CarrierHeatpumpIR.h>
+#include <ToshibaDaiseikaiHeatpumpIR.h>
 IRSenderPWM irSender(9);     // IR led on Duemilanove digital pin 3, using Arduino PWM
 //IRSenderBlaster irSender(9); // IR led on Duemilanove digital pin 3, using IR Blaster (generates the 38 kHz carrier)
 
-DaiseikaiHeatpumpIR *heatpumpIR;
+ToshibaDaiseikaiHeatpumpIR *heatpumpIR;
 
 // DHT
 #include "DHT.h"
@@ -386,7 +386,7 @@ void setup()
   dht.begin();
   delay(1000);
 
-  heatpumpIR = new DaiseikaiHeatpumpIR();
+  heatpumpIR = new ToshibaDaiseikaiHeatpumpIR();
 
   Serial.println(F("Running"));
 }
